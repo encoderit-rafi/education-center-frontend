0fa23160e551bbe9881617d7799d5dfe107aa538
encoderit-rafi <rafi@encoderit.net>
2026-05-07 18:58:35 +0600

    footer

--- src/components/blocks/banner.tsx
+++ src/components/blocks/banner.tsx
@@ -1,9 +1,15 @@
 import { BadgeCheck } from "lucide-react";
+import { Marquee } from "../ui/marquee";
 
 export default function Banner() {
   return (
     <div className="hidden md:block bg-primary text-white">
-      <div className="container mx-auto flex h-8 items-center justify-center base-px text-[11px]">
+      {/* <div className="container mx-auto flex h-8 items-center justify-center base-px text-[11px]">
+      </div> */}
+      <Marquee
+        pauseOnHover
+        className="[--duration:15s] max-w-3xl overflow-hidden mx-auto flex h-8 items-center justify-center base-px text-[11px]"
+      >
         <div className="flex items-center gap-2 text-white">
           <BadgeCheck className="text-yellow-300 size-3" />
           <span className="font-medium tracking-wide">
@@ -12,7 +18,7 @@ export default function Banner() {
             English (SELT)
           </span>
         </div>
-      </div>
+      </Marquee>
     </div>
   );
 }
