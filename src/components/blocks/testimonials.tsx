c3aa1581357aa9935e9c8acd905a9bf116ea3ceb
encoderit-khaled <khaled@encoderit.net>
2026-05-17 10:53:46 +0600

    update-all

--- src/components/blocks/testimonials.tsx
+++ src/components/blocks/testimonials.tsx
@@ -132,7 +132,6 @@ function TestimonialCard({
             </span>
           ))}
         </div>
-        <GoogleLogo />
       </div>
 
       <p className="text-gray-800 text-lg font-medium leading-relaxed line-clamp-4">
