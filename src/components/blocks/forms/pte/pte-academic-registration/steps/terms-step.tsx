e4ed865285b955a7050d54c086895986b52dd6b8
encoderit-khaled <khaled@encoderit.net>
2026-08-27 12:46:45 +0600

    update-pte

--- src/components/blocks/forms/pte/pte-academic-registration/steps/terms-step.tsx
+++ src/components/blocks/forms/pte/pte-academic-registration/steps/terms-step.tsx
@@ -14,11 +14,9 @@ interface TermsStepProps {
 
 export function TermsStep({ onNext, examFee, additionalFee }: TermsStepProps) {
   const t = useTranslations("TermsSteps");
-  const examName = "PTE Academic";
 
   const notices: (React.ReactNode | string)[] = Array.from({ length: 23 }).map((_, idx) => {
-    return t.rich(`pte.${idx}`, {
-      examName: examName,
+    return t.rich(`pteAcademic.${idx}`, {
       aed_exam: () => (
         <strong className="font-semibold inline-flex items-center gap-1"><AED className="h-[0.8em] w-auto fill-current" /> {examFee.toLocaleString()}</strong>
       ),
